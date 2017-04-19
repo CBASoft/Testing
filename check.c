8009525678166229f6a1064954bcdacf7fcb1e0c
CBASoft <CBASoft@users.noreply.github.com>
2017-04-19 14:17:57 +0100

    Quick compiler test.
    Basically wrote this after.... 7 years... Wow. To see if I still remembered any C at all. Turns out I did.
    
    Compiles with gcc -Wall check.c -o out.a

---
+++ check.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int $check1 = 1;
+int $check2 = 1;
+if ($check1 == $check2) {
+	printf("Random check.\n\"Assuming\" all goes to plan. That is and if it works.\n");
+	getchar();
+	return(0);
+	}
+}
